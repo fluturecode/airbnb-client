7dcb83955b5f8eb3e026d5fc0f5c156840e8c0c6
fluturecode <fluturecode@gmail.com>
2020-09-01 14:49:31 -0400

    finished all routes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,25 @@ import "./App.css";
 import Home from "./Components/Home";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import SearchPage from "./Components/SearchPage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
 	return (
 		<div className="app">
-			<Header />
-			<Home />
-			<Footer />
+			<Router>
+				<Header />
+				<Switch>
+					<Route path="/search">
+						<SearchPage />
+					</Route>
+					<Route path="/">
+						<Home />
+					</Route>
+				</Switch>
+
+				<Footer />
+			</Router>
 		</div>
 	);
 }
