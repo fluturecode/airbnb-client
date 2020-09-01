c9180f69438ebc203f8df480dbcecde5a3428e03
fluturecode <fluturecode@gmail.com>
2020-09-01 13:17:31 -0400

    search dates feature

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -23,9 +23,9 @@ function Banner() {
 				</Button>
 			</div>
 			<div className="banner__info">
-				<h1>Get out and stretch your imagination</h1>
+				<h1>Discover your next home away from home...</h1>
 				<h5>
-					Plan a different kind of getaway to uncover the hidden gems near you.
+					Plan a different kind of getaway to uncover the hidden gems near you
 				</h5>
 				<Button onClick={() => history.push("/search")} variant="outlined">
 					Explore Nearby
