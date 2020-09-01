38919c71ba61ffa44a59ee20651663765cedbcc4
fluturecode <fluturecode@gmail.com>
2020-09-01 14:27:13 -0400

    build - footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Home from "./Components/Home";
 import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 function App() {
 	return (
 		<div className="app">
 			<Header />
 			<Home />
+			<Footer />
 		</div>
 	);
 }
