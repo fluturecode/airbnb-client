4e7dede1b6713ead2cd03fb57a8dc76ab72ee564
Elizabeth Eidelson <fluturecode@gmail.com>
2021-09-13 16:14:05 +0200

    modified src/Components/Banner.css src/Components/Map.js

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -2,6 +2,9 @@ import ReactMapGL from 'react-map-gl';
 
 export const Map = () => {
   return (
-  <ReactMapGL></ReactMapGL>
+  <ReactMapGL
+  mapStyle='mapbox://styles/fluturecode/cktip20cp0sj817qlkzcfbqwx'
+  mapboxApiAccessToken={process.env.mapbox_key}>
+  </ReactMapGL>
   )
 }
